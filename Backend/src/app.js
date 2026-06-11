07174ca91dcf78f1cd7aedea74f72d2bf54aa398
Priyam-77818 <161926678+Priyam-77818@users.noreply.github.com>
2026-06-11 18:05:54 +0530

    fix: update CORS for vercel URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ["http://localhost:5173", "https://interview-ai-ten.vercel.app"],
+    origin: ["http://localhost:5173", "https://interview-ai-tau-sand.vercel.app"],
     credentials: true
 }))
 
